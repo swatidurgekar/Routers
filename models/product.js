9a730418fc64e640bbc77a803b9081a18a0bab1f
swatidurgekar <swatidurgekar@gmail.com>
2023-08-26 21:51:54 +0530

    Dynamic Routes Part 3 (Query Params)

--- models/product.js
+++ models/product.js
@@ -17,8 +17,21 @@ const getProductsFromFile = (cb) => {
   });
 };
 
+const deleteProductById = (id) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (!err) {
+      let content = JSON.parse(fileContent);
+      let updatedProducts = content.filter((prod) => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        console.log(err);
+      });
+    }
+  });
+};
+
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +39,23 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
@@ -44,5 +68,9 @@ module.exports = class Product {
       const product = products.find((p) => p.id === id);
       cb(product);
     });
+  }
+
+  static deleteProduct(id) {
+    deleteProductById(id);
   }
 };
